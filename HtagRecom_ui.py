c9b0877b08dde6f4d9567230b2376d96a41df513
Noel Joe Basil <67711280+noeljbasil@users.noreply.github.com>
2023-03-13 23:26:59 +0000

    Update HtagRecom_ui.py

--- HtagRecom_ui.py
+++ HtagRecom_ui.py
@@ -4,6 +4,7 @@ from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
 from langchain.agents import load_tools
 from langchain import PromptTemplate
+from langchain import LLMChain
 
 header      = st.container()
 description = st.container()
@@ -14,25 +15,39 @@ def generate_hashtags(tweet,number):
     llm = OpenAI(temperature=1)
     tool_names = ["serpapi"]
     tools = load_tools(tool_names)
-    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=False)
+    davinci = OpenAI(model_name='text-davinci-003')
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
     
     template = """ Generate {number} unique creative hashtags that are relevant to the tweet provided below so that the tweet is trending. The user of the hashtags is a 
     non profit organization called Stop The Traffik (STT) who is helping end human trafficking across the globe and hence the hashtags generated needs to be inline with that of a professional media outlet. 
     If no relevant hashtags can be generated, answer with "No hashtags could be generated for this tweet. Apologies for the inconvenience".
-
     Tweet: {tweet}
-
     Hashtags: """
 
     prompt_template = PromptTemplate(
         input_variables=["tweet","number"],
         template=template
     )
-    recommended_hashtags = agent.run(prompt_template.format(
-                                    tweet=tweet,
-                                    number = number_of_hashtags
-                                ))
 
+    try:
+        recommended_hashtags = agent.run(prompt_template.format(
+                                        tweet=tweet,
+                                        number = number
+                                    ))
+        if recommended_hashtags == "No hashtags could be generated for this tweet. Apologies for the inconvenience." or recommended_hashtags == "Agent stopped due to max iterations.":
+            llm_chain = LLMChain(
+                                    prompt=prompt_template,
+                                    llm=davinci
+                                )
+            recommended_hashtags = llm_chain.run({'tweet':tweet,'number':number})
+        else:
+            pass
+    except:
+        llm_chain = LLMChain(
+                                    prompt=prompt_template,
+                                    llm=davinci
+                                )
+        recommended_hashtags = llm_chain.run({'tweet':tweet,'number':number})
     return (recommended_hashtags)
 
 def main():
@@ -41,22 +56,24 @@ def main():
         st.markdown('<p class="title">Hashtag Recommender for tweets</p>', unsafe_allow_html=True)
 
     with description:
-        st.markdown(""" <style> .desc { font-size:15px ; font-family: 'Futura'; color: #ffffff;} </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="desc">This tool is developed to help the user generate relevant hashtags for any tweet</p>', unsafe_allow_html=True)
+        st.markdown(""" <style> .desc { font-size:15px ; font-family: 'Futura'; color: #000000;} </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="desc">This tool is developed to help the user generate relevant hashtags for tweets</p>', unsafe_allow_html=True)
 
     with features:
         tweet                   = {}
         tweet['text']           =  st.text_area('Enter the tweet text')
         tweet['hashtag_number'] =  st.selectbox('Enter the number of hashtags to be generated',( '0', '1', '2', '3','4', '5', '6', '7','8', '9', '10'),10) 
     with output:
-        st.markdown(""" <style> .outp { font-size:15px ; font-family: 'Futura'; color: #ffffff;} </style> """, unsafe_allow_html=True)
+        st.markdown(""" <style> .outp { font-size:15px ; font-family: 'Futura'; color: #000000;} </style> """, unsafe_allow_html=True)
         st.markdown('<p class="outp">Recommended hashtags are:</p>', unsafe_allow_html=True)
 
-    if tweet['text'] is not "":
-        if st.button('Click here to generate hashtags'):    
-            with output:
-                hashtags = generate_hashtags(tweet['text'],tweet['hashtag_number'])
-                st.write(hashtags)
-    
+    if st.button('Click here to generate hashtags'):
+        if tweet['text'] != "":
+            with st.spinner("Generating hashtags..."):
+                with output:
+                    hashtags = generate_hashtags(tweet['text'],tweet['hashtag_number'])
+                    st.write(hashtags)
+        else:
+            st.error("Enter tweet to proceed")
 if __name__ == "__main__":
     main() 
